5b83ab89e2e6f462aa7290b3d096c07c31109e40
yntymak <kubanychevyntymak21@gmail.com>
2024-10-29 12:12:24 +0600

    customized react query

--- src/config/url.config.ts
+++ src/config/url.config.ts
@@ -1,1 +1,15 @@
 export const APP_URL = `${process.env.SERVER_URL}/api` as string;
+
+export const PUBLIC_URL = {
+  root: (url = "") => `${url ? url : ""}`,
+  home: () => PUBLIC_URL.root("/"),
+  auth: () => PUBLIC_URL.root("/auth"),
+  explorer: () => PUBLIC_URL.root("/explorer"),
+  trending: () => PUBLIC_URL.root("/trending"),
+  actor: (slug = "") => PUBLIC_URL.root(`/actor/${slug}`),
+  genre: (slug = "") => PUBLIC_URL.root(`/genre/${slug}`),
+  movie: (slug = "") => PUBLIC_URL.root(`/movie/${slug}`),
+  premium: () => PUBLIC_URL.root("/premium"),
+};
+export const DASHBOARD_URL = {};
+export const ADMIN_URL = {};
