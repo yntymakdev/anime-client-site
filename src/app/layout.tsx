a36007c70946ee4562a1f84ee645a1fe2031d015
yntymak <kubanychevyntymak21@gmail.com>
2024-10-29 11:36:02 +0600

    created and finish metada ^^

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,6 @@
 import type { Metadata } from "next";
-// import localFont from "next/font/local";
-import { GeistSans } from "geist/font/sans";
-import { GeistMono } from "geist/font/mono";
-import "./globals.css";
-import Providers from "./providers";
-
-// const geistSans = localFont({
+//  ?import localFont from "next/font/local";
+//? const geistSans = localFont({
 //   src: "./fonts/GeistVF.woff",
 //   variable: "--font-geist-sans",
 //   weight: "100 900",
@@ -15,10 +10,24 @@ import Providers from "./providers";
 //   variable: "--font-geist-mono",
 //   weight: "100 900",
 // });
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import "./globals.css";
+import Providers from "./providers";
+import { SITE_DESCRIPTION, SITE_NAME } from "./constants/seo.constants";
 
 export const metadata: Metadata = {
-  title: "YNTY PORTFOLIO",
-  description: "HI there",
+  title: {
+    absolute: SITE_NAME,
+    template: `%s | ${SITE_NAME}`,
+  },
+  description: SITE_DESCRIPTION,
+  metadataBase: new URL(process.env.APP_URL as string),
+  openGraph: {
+    type: "website",
+    siteName: SITE_NAME,
+    emails: ["info@yumestories.com"],
+  },
 };
 
 export default function RootLayout({
