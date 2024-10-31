ce81815201fe6e4e9f94fd1c14d65dfe25396661
yntymak <kubanychevyntymak21@gmail.com>
2024-10-31 20:45:21 +0600

    added full actor service

--- src/services/actor.service.ts
+++ src/services/actor.service.ts
@@ -1,4 +1,4 @@
-import { axiosClassic } from "@/api/interceptors";
+import { axiosClassic, axiosWithAuth } from "@/api/interceptors";
 import { API_URL } from "@/config/api.config";
 import { IActor } from "@/types/actor.types";
 
@@ -13,5 +13,23 @@ class ActorService {
     });
 
     return data;
+  }
+  async getById(id: string) {
+    return axiosWithAuth.get<IActor>(API_URL.actors(`/by-id/${id}`));
+  }
+  async getBySlug(slug: string) {
+    return axiosClassic.get<IActor>(API_URL.actors(`/by-slug/${slug}`));
+  }
+
+  async create() {
+    return axiosWithAuth.post<string>(API_URL.actors(""));
+  }
+
+  async update(id: string, data: IActorEditInput) {
+    return axiosWithAuth.put<string>(API_URL.actors(`/${id}`), data);
+  }
+
+  async delete(id: string) {
+    return axiosWithAuth.delete<string>(API_URL.actors(`/${id}`));
   }
 }
