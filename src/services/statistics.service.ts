c70a775a7ac07d49deeb2ecc8f88e676384c22f2
yntymak <kubanychevyntymak21@gmail.com>
2024-10-31 20:57:21 +0600

    added payment service full

--- src/services/statistics.service.ts
+++ src/services/statistics.service.ts
@@ -4,13 +4,13 @@ import { API_URL } from "@/config/api.config";
 import { IMiddleStatisticsResponse } from "@/types/statisctics.types";
 
 class StatisticsService {
-  async getMain() {
-    const { data } = await axiosWithAuth.get<IStatisticItem[]>(
-      API_URL.statistics("/main"),
-    );
+  //   async getMain() {
+  //     const { data } = await axiosWithAuth.get<IStatisticItem[]>(
+  //       API_URL.statistics("/main"),
+  //     );
 
-    return data;
-  }
+  //     return data;
+  //   }
 
   async getMiddle() {
     const { data } = await axiosWithAuth.get<IMiddleStatisticsResponse>(
