ee11045184091de6a1eaa6962237243c999265f3
yntymak <kubanychevyntymak21@gmail.com>
2024-11-09 19:44:23 +0600

    creating middleware

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,7 +1,7 @@
 import React, { FC, PropsWithChildren } from "react";
 
 const Layout: FC<PropsWithChildren<unknown>> = ({ children }) => {
-  return <div>{children}</div>;
+  return <div>Layout{children}</div>;
 };
 
 export default Layout;
