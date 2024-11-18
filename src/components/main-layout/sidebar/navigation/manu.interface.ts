2e606870d9382a96d595b9d2a84988f5c69bdcf0
yntymak <kubanychevyntymak21@gmail.com>
2024-11-18 16:25:54 +0600

    i am back

--- src/components/main-layout/sidebar/navigation/manu.interface.ts
+++ src/components/main-layout/sidebar/navigation/manu.interface.ts
@@ -10,3 +10,27 @@ export interface IMenu {
   title: string;
   items: IMenuItem;
 }
+import { ADMIN_URL, PUBLIC_URL } from "@/config/url.config";
+
+import { IMenu } from "./menu.interface";
+
+export const userMenu: IMenu = {
+  title: "Меню",
+  items: [
+    {
+      icon: "LuCompass",
+      link: PUBLIC_URL.home(),
+      value: "Главная",
+    },
+    {
+      icon: "LuClapperboard",
+      link: PUBLIC_URL.explorer(),
+      value: "Фильмы",
+    },
+    {
+      icon: "LuFlame",
+      link: PUBLIC_URL.trending(),
+      value: "Популярные",
+    },
+  ],
+};
